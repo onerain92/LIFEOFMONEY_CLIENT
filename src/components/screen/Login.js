ad7603da745ce0f6bc11e09cca4540eef3a4c092
ILWOO <dlfdn91@gmail.com>
2019-11-05 21:28:03 +0900

    add Login function and icons svg

--- src/components/screen/Login.js
+++ src/components/screen/Login.js
@@ -1,22 +1,68 @@
-// @flow
+// @flow
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  Image,
+} from 'react-native';
+import {LoginManager} from 'react-native-fbsdk';
 import {APP_LOGO} from '../../utils/Images';
-import {colors} from '../../utils/Styles';
+import {Facebook} from '../../../assets/icons/Icons';
 
-function Login(props) {
+const Login = props => {
+  const fbAuth = () => {
+    LoginManager.logInWithPermissions(['public_profile']).then(
+      function(result) {
+        if (result.isCancelled) {
+          console.log('Login was cancelled');
+        } else {
+          console.log(
+            'Login was successful with permissions: ' +
+              result.grantedPermissions.toString(),
+          );
+        }
+      },
+      function(error) {
+        console.log('Login failed with error: ' + error);
+      },
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>로그인 페이지.</Text>
+      <Image source={APP_LOGO} style={styles.logo} />
+      <Facebook />
+      <TouchableOpacity onPress={fbAuth}>
+        <View style={styles.loginBtn}>
+          <Text style={styles.loginTxt}>페이스북으로 시작하기</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFE555',
+  },
+  logo: {
+    width: 280,
+    height: 80,
+  },
+  loginBtn: {
+    padding: 10,
+    backgroundColor: '#4267B2',
+    borderRadius: 5,
+  },
+  loginTxt: {
+    fontSize: 20,
+    color: '#ffffff',
   },
 });
 
